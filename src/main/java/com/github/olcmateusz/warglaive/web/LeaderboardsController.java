ddeb499265acd38894fa8bdadb4d420e025431d8
Mateusz Olczak <olc.mateusz@gmail.com>
2023-11-23 12:29:16 +0100

    fix ulrs

--- src/main/java/com/github/olcmateusz/warglaive/web/LeaderboardsController.java
+++ src/main/java/com/github/olcmateusz/warglaive/web/LeaderboardsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -44,10 +45,11 @@ public class LeaderboardsController {
 		}
 
 
-
-
-//    @Autowired
-//    private PlayerRepository playerRepo;
+	@GetMapping("{region}/{bracket}")
+	public String params(@PathVariable String region, @PathVariable String bracket) {
+		System.out.println(region + " " + bracket);
+		return "leaderboards";
+	}
 	
 	
 	
